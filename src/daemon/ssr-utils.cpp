a36339167a50c43c1037608735206d4243f2f567
tangjie02 <tangjie02@kylinos.com.cn>
2021-08-25 08:57:29 +0800

    feature(plugin): Support python plugin.
    - 支持python插件
    
    Signed-off-by: tangjie02 <tangjie02@kylinos.com.cn>

--- src/daemon/ssr-utils.cpp
+++ src/daemon/ssr-utils.cpp
@@ -4,9 +4,22 @@
  * @author        tangjie02 <tangjie02@kylinos.com.cn>
  * @copyright (c) 2020~2021 KylinSec Co., Ltd. All rights reserved. 
  */
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
 
+#include "lib/base/base.h"
 #include "src/daemon/ssr-utils.h"
 
 namespace Kiran
 {
+std::string SSRUtils::PyUnicode_AsString(PyObject *unicode)
+{
+    auto bytes = PyUnicode_AsASCIIString(unicode);
+    RETURN_VAL_IF_FALSE(bytes, std::string());
+    SCOPE_EXIT({
+        Py_XDECREF(bytes);
+    });
+    auto str = PyBytes_AsString(bytes);
+    return POINTER_TO_STRING(str);
+}
 }  // namespace Kiran
