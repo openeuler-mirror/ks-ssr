9177c6dd61e74f4c4df9c4ae6fe1c19b06917772
tangjie02 <tangjie02@kylinsec.com.cn>
2021-11-12 18:02:43 +0800

    feature(plugin): Add some reinforcements.
    - 添加一些加固项
    
    Signed-off-by: tangjie02 <tangjie02@kylinsec.com.cn>

--- src/tool/config/pam.h
+++ src/tool/config/pam.h
@@ -16,6 +16,22 @@ namespace Config
 class PAM
 {
 public:
+    struct MatchLineInfo
+    {
+        std::string content;
+        std::string match_line;
+        int32_t match_pos;
+        bool is_match_comment;
+        MatchLineInfo() : match_pos(0), is_match_comment(false) {}
+    };
+
+    /**
+     * @brief 创建PAM对象
+     * @param {conf_path} PAM配置路径。
+     * @param {line_match_pattern}  需要操作的行，如果有多个行都匹配，则随机选择一个
+     * @param {fallback_line} 如果未找到匹配行，则使用传入的缺省行
+     * @return {} 
+     */
     PAM(const std::string &conf_path,
         const std::string &line_match_pattern);
 
@@ -26,15 +42,28 @@ public:
      * @param {value}  如果kv_split_pattern设置为空，则value表示key是否存在（被设置为"true"或者"false")，否则value为获取到的值。
      * @return {} 如果出错则返回false,否则返回true
      */
-    bool get(const std::string &key, const std::string &kv_split_pattern, std::string &value);
+    bool get_value(const std::string &key, const std::string &kv_split_pattern, std::string &value);
 
-    bool set(const std::string &key,
-             const std::string &kv_split_pattern,
-             const std::string &value,
-             const std::string &kv_join_str);
-    bool del(const std::string &key, const std::string &kv_split_regex);
+    bool set_value(const std::string &key,
+                   const std::string &kv_split_pattern,
+                   const std::string &value,
+                   const std::string &kv_join_str);
+
+    bool del_value(const std::string &key, const std::string &kv_split_pattern);
+
+    // 如果没有找到匹配行，则将fallback_line添加到末尾
+    bool add_line(const std::string &fallback_line);
+    // 删除匹配行
+    bool del_line();
+    // 获取匹配行
+    bool get_line(std::string &line);
 
 private:
+    PAM::MatchLineInfo get_match_line();
+
+    // 将内容写入文件
+    bool write_to_file(const std::string &content);
+
     // 最后一个字符是否为空白字符
     bool is_whitespace_in_tail(const std::string &str);
 
