da54a0b2d3c0b5ea6d6e562016f60c0008d02a87
chendingjian <chendingjian@kylinos.com.cn>
2022-02-23 16:41:24 +0800

    fix(plugins): Fixed ordered a number of repair history bug. - 修复历史命令条数修改不正确，以及配置不生效的问题 - #49641
    Signed-off-by: chendingjian <chendingjian@kylinos.com.cn>

--- src/tool/config/kv.cpp
+++ src/tool/config/kv.cpp
@@ -188,6 +188,110 @@ bool KV::set(const std::string &key, const std::string &value)
     return true;
 }
 
+bool KV::set_all(const std::string &key, const std::string &value)
+{
+    std::string new_contents;
+
+    // 在读写期间都不应该让其他进程改动该文件，否则可能会导致结果不一致。
+    auto file_lock = FileLock::create_excusive_lock(this->conf_path_, O_RDWR | O_CREAT | O_SYNC, CONF_FILE_PERMISSION);
+    if (!file_lock)
+    {
+        KLOG_WARNING("Failed to lock file %s.", this->conf_path_.c_str());
+        return false;
+    }
+
+    auto contents = Glib::file_get_contents(this->conf_path_);
+    auto lines = StrUtils::split_lines(contents);
+
+    auto split_field_regex = Glib::Regex::create(this->kv_split_pattern_, Glib::RegexCompileFlags::REGEX_OPTIMIZE);
+    auto second_field_regex = Glib::Regex::create(fmt::format("(\\s*\\S+{0})(\\S+)", this->kv_split_pattern_),
+                                                  Glib::RegexCompileFlags::REGEX_OPTIMIZE);
+
+    int32_t match_pos = 0;
+    bool is_match_comment = false;
+    bool is_matching = false;
+
+    // 寻找匹配key的行，如果没有匹配的非注释行可用，则使用匹配的注释行（注释将被去掉）
+    for (const auto &line : lines)
+    {
+        std::string match_line = "";
+        std::vector<std::string> fields;
+
+        // 注释行判断需要包括前面的空白字符
+        bool is_comment = StrUtils::startswith(line, this->comment_);
+
+        if (is_comment)
+        {
+            auto trim_line = StrUtils::trim(line.substr(this->comment_.size()));
+            fields = split_field_regex->split(trim_line);
+        }
+        else
+        {
+            auto trim_line = StrUtils::trim(line);
+            fields = split_field_regex->split(trim_line);
+        }
+
+        if (fields.size() == 2 &&
+            fields[0] == key &&
+            (match_line.size() == 0 || (int32_t(is_comment) <= int32_t(is_match_comment))))
+        {
+            match_pos = new_contents.size();
+            match_line = line;
+            is_match_comment = is_comment;
+        }
+
+        new_contents.append(line);
+        new_contents.push_back('\n');
+
+        if (match_line.size() > 0)
+        {
+            // 匹配成功标志位，如果匹配到，就不添加新的参数
+            is_matching = true;
+            if (value.empty())
+            {
+                // 如果不是注释行，则进行注释，否则不处理
+                if (!is_match_comment)
+                {
+                    new_contents.replace(match_pos, match_line.size(), this->comment_ + match_line);
+                    KLOG_DEBUG("Comment line: %s with %s.", match_line.c_str(), this->comment_.c_str());
+                }
+            }
+            else
+            {
+                // 如果存在注释则去掉，然后对value进行修改
+                auto uncomment_line = is_match_comment ? match_line.substr(this->comment_.size()) : match_line;
+                auto replace_line = second_field_regex->replace(uncomment_line, 0, "\\g<1>" + value, static_cast<Glib::RegexMatchFlags>(0));
+                new_contents.replace(match_pos, match_line.size(), replace_line);
+                KLOG_DEBUG("contents: %s.", new_contents.c_str());
+                KLOG_DEBUG("Replace line: %s with %s.", match_line.c_str(), replace_line.c_str());
+            }
+        }
+    }
+
+    if(!is_matching)
+    {
+        // 如果不存在匹配行且value不为空，则在最后添加一行
+        if (!value.empty())
+        {
+            auto new_line = key + this->kv_join_str_ + value + "\n";
+            KLOG_DEBUG("New line: %s.", new_line.c_str());
+            new_contents.append(new_line);
+        }
+    }
+
+    try
+    {
+        KLOG_DEBUG("New contents: %s.", new_contents.c_str());
+        FileUtils::write_contents(this->conf_path_, new_contents);
+    }
+    catch (const Glib::Error &e)
+    {
+        KLOG_WARNING("Failed to write file contents: %s.", this->conf_path_.c_str());
+        return false;
+    }
+    return true;
+}
+
 bool KV::del(const std::string &key)
 {
     // 这里偷了一个懒，合理做法是需要单独实现删除逻辑，set函数不应该执行删除操作
