4fe95823a7b3e60013caff2317d4b1040b04d356
pengyulong <pengyulong@kylinsec.com.cn>
2022-03-15 11:35:52 +0800

    fix(logfile): Fix log file permissions.
    - 修复日志文件权限问题
    - 增加重要文件权限限制项
    - #50806、#50885、#49652、#50768、#50893、#50886
    
    Signed-off-by: pengyulong <pengyulong@kylinsec.com.cn>

--- plugins/python/ssr/audit/logfile.py
+++ plugins/python/ssr/audit/logfile.py
@@ -11,6 +11,7 @@ import ssr.utils
 import json
 import ssr.vars
 
+LOGFILE_CONF_FILEPATH = '/etc/logrotate.d/syslog'
 LOGFILE_INI_FILEPATH = ssr.vars.SSR_PLUGIN_PYTHON_ROOT_DIR + "/ssr/audit/logfile.ini"
 LOGFILE_GROUP_PERMISSIONS = "Permissions"
 # LPK: Logfile Permissions Key
@@ -22,10 +23,11 @@ EXCLUDE_MODE = stat.S_IWGRP | stat.S_IXGRP | stat.S_IWOTH | stat.S_IXOTH | stat.
 PERMISSIONS_ARG_MODE_PERMISSIONS_LIMIT = "mode-permissions-limit"
 PERMISSIONS_ARG_APPEND_PERMISSIONS_LIMIT = "append-permissions-limit"
 
-LOGFILE_ROTETE_CONF = '/var/log/messages\n{\n    missingok\n\
+LOGFILE_ROTETE_CONF = '### KSSRManager ###\n/var/log/messages\n{\n    missingok\n\
     sharedscripts\n    prerotate\n        /usr/bin/chattr -a /var/log/messages\n\
         endscript\n    postrotate\n        /usr/bin/systemctl kill -s HUP rsyslog.service >/dev/null 2>&1 || true\n\
-                /usr/bin/chattr +a /var/log/messages\n    endscript\n}'
+                /usr/bin/chattr +a /var/log/messages\n    endscript\n}\n### KSSRManager ###'
+
 
 # 系统日志文件和配置的权限控制
 class Permissions:
@@ -69,11 +71,12 @@ class Permissions:
         # ssr.log.debug(append_permissions_limit)
         #retdata[PERMISSIONS_ARG_APPEND_PERMISSIONS_LIMIT] = append_permissions_limit
 
-        output = ssr.utils.subprocess_has_output('grep -r {0} {1}'.format('/var/log/messages','/etc/logrotate.d/syslog'))
-        if len(output) == 0:
-            retdata[PERMISSIONS_ARG_APPEND_PERMISSIONS_LIMIT] = False
-        else:
+        output = ssr.utils.subprocess_has_output('grep -r "{0}" {1}'.format('/var/log/messages',LOGFILE_CONF_FILEPATH))
+        output_kssrmanager = ssr.utils.subprocess_has_output('grep -r "{0}" {1}'.format('### KSSRManager ###', LOGFILE_CONF_FILEPATH))
+        if len(output) != 0 and len(output_kssrmanager) != 0:
             retdata[PERMISSIONS_ARG_APPEND_PERMISSIONS_LIMIT] = True
+        else:
+            retdata[PERMISSIONS_ARG_APPEND_PERMISSIONS_LIMIT] = False
 
         return (True, json.dumps(retdata))
 
@@ -98,9 +101,17 @@ class Permissions:
         #         ssr.utils.subprocess_not_output('chattr +a {0}'.format(append_file))
 
         if args[PERMISSIONS_ARG_APPEND_PERMISSIONS_LIMIT]:
-            output = ssr.utils.subprocess_has_output('grep -r {0} {1}'.format('/var/log/messages','/etc/logrotate.d/syslog'))
-            if len(output) == 0:
-                ssr.utils.subprocess_not_output('echo \'{0}\'    >> {1}'.format( LOGFILE_ROTETE_CONF, '/etc/logrotate.d/syslog'))
+            output = ssr.utils.subprocess_has_output('grep -r "{0}" {1}'.format('/var/log/messages',LOGFILE_CONF_FILEPATH))
+            output_kssrmanager = ssr.utils.subprocess_has_output('grep -r "{0}" {1}'.format('### KSSRManager ###', LOGFILE_CONF_FILEPATH))
+            if len(output) == 0 and len(output_kssrmanager) == 0:
+                ssr.utils.subprocess_not_output('echo \'{0}\'    >> {1}'.format( LOGFILE_ROTETE_CONF, LOGFILE_CONF_FILEPATH))
+            elif len(output_kssrmanager) == 0 and len(output) != 0:
+                ssr.utils.subprocess_not_output('sed -i \'s/{0}/ /g\' {1}'.format("\/var\/log\/messages", LOGFILE_CONF_FILEPATH))
+                ssr.utils.subprocess_not_output('echo \'{0}\'    >> {1}'.format( LOGFILE_ROTETE_CONF, LOGFILE_CONF_FILEPATH))
         else:
-            ssr.utils.subprocess_not_output('sed -i \'s/{0}/{1}/g\' {2}'.format(LOGFILE_ROTETE_CONF, ' ', '/etc/logrotate.d/syslog'))
+            output = ssr.utils.subprocess_has_output('grep -rn "{0}" {1} | cut -f1 -d:'.format('### KSSRManager ###', LOGFILE_CONF_FILEPATH))
+            if len(output) != 0:
+                line = output.split()
+                ssr.utils.subprocess_not_output('sed -i \'{0},{1}d\' {2}'.format(line[0], line[1], LOGFILE_CONF_FILEPATH))
+
         return (True, '')
