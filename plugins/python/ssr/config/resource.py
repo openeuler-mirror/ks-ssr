17a89f81038d57cf2bfe5156b081d632bba857d7
chendingjian <chendingjian@kylinos.com.cn>
2022-08-08 15:19:38 +0800

    fix(resource) : Fix bugs with critical resource constraints. - 修复关键资源限制的bug(#59307)
    Signed-off-by: chendingjian <chendingjian@kylinos.com.cn>

--- plugins/python/ssr/config/resource.py
+++ plugins/python/ssr/config/resource.py
@@ -2,12 +2,14 @@
 
 import json
 import re
+import os
 import ssr.utils
 import ssr.configuration
 import ssr.log
 
 RESOURCE_LIMITS_CONF_PATH = "/etc/security/limits.d/90-ssr-config.conf"
 SELINUX_MODULES_ULIMIT_PATH = "/usr/share/ks-ssr-manager/ssr-ulimit.pp"
+PAM_CHECK_PATH = "/etc/security/limits.d/20-nproc.conf"
 
 RESOURCE_LIMITS_KEY_STACK_SOFT  = "*                soft    stack"
 RESOURCE_LIMITS_KEY_STACK_HARD  = "*                hard    stack"
@@ -30,6 +32,7 @@ HISTORY_SIZE_LIMIT_CONF_KEY_HISTSIZE_EXPORT = "export HISTSIZE"
 class ResourceLimits:
     def __init__(self):
         self.conf = ssr.configuration.KV(RESOURCE_LIMITS_CONF_PATH)
+        self.conf_stack = ssr.configuration.KV(HISTORY_SIZE_LIMIT_CONF_BASHRC_PATH)
 
     def get_selinux_status(self):
         output = ssr.utils.subprocess_has_output("getenforce")
@@ -79,6 +82,10 @@ class ResourceLimits:
             self.conf.set_value(RESOURCE_LIMITS_KEY_STACK_HARD, '10240')
             self.conf.set_value(RESOURCE_LIMITS_KEY_RSS_SOFT, '10240')
             self.conf.set_value(RESOURCE_LIMITS_KEY_RSS_HARD, '10240')
+            if not os.path.exists(PAM_CHECK_PATH):
+                ssr.utils.subprocess_not_output("sed -i '/{0}/d' {1}".format('ulimit', HISTORY_SIZE_LIMIT_CONF_BASHRC_PATH))
+                self.conf_stack.set_value(RESOURCE_LIMITS_STACK_CMD, '8192')
+                self.conf_stack.set_value(RESOURCE_LIMITS_RSS_CMD, '10240')
         else:
             if self.get_selinux_status():
                 ssr.utils.subprocess_not_output("semodule -i {0}".format(SELINUX_MODULES_ULIMIT_PATH))
@@ -86,6 +93,10 @@ class ResourceLimits:
             self.conf.set_value(RESOURCE_LIMITS_KEY_STACK_HARD, 'unlimited')
             self.conf.set_value(RESOURCE_LIMITS_KEY_RSS_SOFT, 'unlimited')
             self.conf.set_value(RESOURCE_LIMITS_KEY_RSS_HARD, 'unlimited')
+            if not os.path.exists(PAM_CHECK_PATH):
+                ssr.utils.subprocess_not_output("sed -i '/{0}/d' {1}".format('ulimit', HISTORY_SIZE_LIMIT_CONF_BASHRC_PATH))
+                self.conf_stack.set_value(RESOURCE_LIMITS_STACK_CMD, 'unlimited')
+                self.conf_stack.set_value(RESOURCE_LIMITS_RSS_CMD, 'unlimited')
 
 
         return (True, '')
