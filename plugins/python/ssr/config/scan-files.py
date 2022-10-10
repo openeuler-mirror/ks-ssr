0ed4fed0372ca06042c22a871f2af98be2f1dc20
chendingjian <chendingjian@kylinos.com.cn>
2022-10-10 14:05:11 +0800

    fix(*) : Modify the data transfer mode. - 修改非法数据的传输方式
    Signed-off-by: chendingjian <chendingjian@kylinos.com.cn>

--- plugins/python/ssr/config/scan-files.py
+++ plugins/python/ssr/config/scan-files.py
@@ -17,78 +17,78 @@ SCAN_FILES_CMD = "find"
 
 class NouserFiles:
     def __init__(self):
-        self.conf = ssr.configuration.Table(SCAN_NOUSER_FILES_RESULT_PATH, ",\\s+")
+        self.is_scan = False
 
     def get(self):
         retdata = dict()
         retdata["enabled"] = False
-        return (True, json.dumps(retdata))
-
-    def set(self, args_json):
-        args = json.loads(args_json)
         scan_output = ""
-        # 无属文件
-        rm_cmd = 'rm -rf {0}'.format(SCAN_NOUSER_FILES_RESULT_PATH)
-        ssr.utils.subprocess_not_output(rm_cmd)
-        if args["enabled"]:
+        if self.is_scan:
             for path in SCAN_FILES_PATH:
                 scan_cmd = '{0} {1} -nouser'.format(SCAN_FILES_CMD,path)
                 tmp_output = ssr.utils.subprocess_has_output(scan_cmd) 
                 if(len(tmp_output)):
                     tmp_output += '\n'
                 scan_output += tmp_output
-                ssr.log.debug("scan_onuser_output = ")
-                ssr.log.debug(scan_output)
-            self.conf.set_value("", scan_output)
+                # ssr.log.debug("scan_onuser_output = ")
+                # ssr.log.debug(scan_output)
+        retdata["nouser_files"] = scan_output
+
+        return (True, json.dumps(retdata))
+
+    def set(self, args_json):
+        args = json.loads(args_json)
+
+        if args["enabled"]:
+            self.is_scan = True
+        else:
+            self.is_scan = False
 
         return (True, '')
 
 class AuthorityFiles:
     def __init__(self):
-        self.conf = ssr.configuration.Table(SCAN_AUTHORITY_FILES_RESULT_PATH, ",\\s+")
+        self.is_scan = False
 
     def get(self):
         retdata = dict()
         retdata["enabled"] = False
-        return (True, json.dumps(retdata))
-
-    def set(self, args_json):
-        args = json.loads(args_json)
         scan_output = ""
-        # 查找指定目录下有777权限的文件
-        rm_cmd = 'rm -rf {0}'.format(SCAN_AUTHORITY_FILES_RESULT_PATH)
-        ssr.utils.subprocess_not_output(rm_cmd)
-        if args["enabled"]:
+        if self.is_scan:
             for path in SCAN_FILES_PATH:
                 scan_cmd = '{0} {1} -perm 777'.format(SCAN_FILES_CMD,path)
                 tmp_output = ssr.utils.subprocess_has_output(scan_cmd) 
                 if(len(tmp_output)):
                     tmp_output += '\n'
                 scan_output += tmp_output
-                ssr.log.debug("scan_777_files_output = ")
-                ssr.log.debug(scan_output)
-            self.conf.set_value("", scan_output)
+                # ssr.log.debug("scan_777_files_output = ")
+                # ssr.log.debug(scan_output)
+        retdata["authority_files"] = scan_output
+
+        return (True, json.dumps(retdata))
+
+    def set(self, args_json):
+        args = json.loads(args_json)
+        
+        if args["enabled"]:
+            self.is_scan = True
+        else:
+            self.is_scan = False
 
         return (True, '')
 
 class SuidSgidFiles:
     def __init__(self):
-        self.conf = ssr.configuration.Table(SCAN_SUID_SGID_FILES_RESULT_PATH, ",\\s+")
+        self.is_scan = False
 
     def get(self):
         retdata = dict()
         retdata["enabled"] = False
-        return (True, json.dumps(retdata))
-
-    def set(self, args_json):
-        args = json.loads(args_json)
         scan_suid_output = ""
         scan_guid_output = ""
-        # /4000表示SUID权限 ，/2000表示GUID权限
-        rm_cmd = 'rm -rf {0}'.format(SCAN_SUID_SGID_FILES_RESULT_PATH)
-        ssr.utils.subprocess_not_output(rm_cmd)
-        if args["enabled"]:
+        if self.is_scan:
             for path in SCAN_FILES_PATH:
+                # /4000表示SUID权限 ，/2000表示GUID权限
                 scan_suid_cmd = '{0} {1} -perm /4000'.format(SCAN_FILES_CMD,path)
                 scan_guid_cmd = '{0} {1} -perm /2000'.format(SCAN_FILES_CMD,path)
                 tmp_suid_output = ssr.utils.subprocess_has_output(scan_suid_cmd) 
@@ -100,9 +100,19 @@ class SuidSgidFiles:
                 
                 scan_suid_output += tmp_suid_output
                 scan_guid_output += tmp_guid_output
-                # ssr.log.debug("scan_777_files_output = ")
-                # ssr.log.debug(scan_output)
-            self.conf.set_value("", "[  SUID ]\n" + scan_suid_output + "[  GUID ]\n" + scan_guid_output)
+
+            retdata["suid_sgid_files"] = "[  SUID ]\n" + scan_suid_output + "[  GUID ]\n" + scan_guid_output 
+        else:
+            retdata["suid_sgid_files"] = ""
+        return (True, json.dumps(retdata))
+
+    def set(self, args_json):
+        args = json.loads(args_json)
+
+        if args["enabled"]:
+            self.is_scan = True
+        else:
+            self.is_scan = False
 
         return (True, '')
             
