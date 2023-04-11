3fd86fa5543996f606b604d1f4bc45cf3166e614
tangjie02 <tangjie02@kylinsec.com.cn>
2023-04-11 08:55:29 +0800

    feature(file-protected): Add rounded corner for QTableView item.
    - 给表格添加圆角
    
    Signed-off-by: tangjie02 <tangjie02@kylinsec.com.cn>

--- src/ui/main.cpp
+++ src/ui/main.cpp
@@ -28,7 +28,7 @@ int main(int argc, char *argv[])
     auto argv0 = QFileInfo(argv[0]);
     auto programName = argv0.baseName();
 
-    if (klog_qt5_init(QString(), "kylinsec-system", PROJECT_NAME, programName) < 0)
+    if (klog_qt5_init(QString(), "kylinsec-session", PROJECT_NAME, programName) < 0)
     {
         fprintf(stderr, "Failed to init kiran-log.");
     }
