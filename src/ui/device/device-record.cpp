ee495a1016c66ee078b211cef5d6ac9905c18fd2
yuanxing <yuanxing@kylinsec.com.cn>
2023-04-26 15:54:00 +0800

    fix(device):improve ui and qss
    - 根据设计图完善ui界面和qss样式

--- src/ui/device/device-record.cpp
+++ src/ui/device/device-record.cpp
@@ -19,6 +19,7 @@
 #include "src/ui/ui_device-record.h"
 
 #include <kiran-log/qt5-log-i.h>
+#include <QHeaderView>
 #include <QPainter>
 #include <QStyleOption>
 
@@ -29,27 +30,12 @@ DeviceRecord::DeviceRecord(QWidget *parent) : QWidget(parent),
 {
     m_ui->setupUi(this);
     m_ui->m_title->setText(tr("Connect Record"));
-
+    m_ui->m_records->setText(tr("0 records in total"));
     m_ui->m_search->addAction(QIcon(":/images/search"), QLineEdit::ActionPosition::LeadingPosition);
+
+    initTableStyle();
+
     connect(m_ui->m_search, &QLineEdit::textChanged, this, &DeviceRecord::searchTextChanged);
-
-    m_ui->m_table->setHeaderSections(QStringList() << tr("Device Name")
-                                                   << tr("Device Type")
-                                                   << tr("Device Time")
-                                                   << tr("Device Status"));
-    m_ui->m_table->setItemDelegate(new DeviceRecordDelegate(this));
-
-    //just test add table data.
-    QList<RecordsInfo> infos;
-    for (int i = 0; i < 50; i++)
-    {
-        auto deviceInfo = RecordsInfo{.name = "1",
-                                      .type = i,
-                                      .time = "1",
-                                      .status = i};
-        infos << deviceInfo;
-    }
-    m_ui->m_table->setData(infos);
 }
 
 DeviceRecord::~DeviceRecord()
@@ -64,6 +50,32 @@ void DeviceRecord::paintEvent(QPaintEvent *event)
     opt.init(this);
     QPainter p(this);
     style()->drawPrimitive(QStyle::PE_Widget, &opt, &p, this);
+}
+
+void DeviceRecord::initTableStyle()
+{
+    m_ui->m_table->setHeaderSections(QStringList() << tr("Device Name")
+                                                   << tr("Device Type")
+                                                   << tr("Device Time")
+                                                   << tr("Device Status"));
+    auto headView = m_ui->m_table->horizontalHeader();
+    headView->resizeSection(RecordTableField::RECORD_TABLE_FIELD_NAME, 200);
+    headView->resizeSection(RecordTableField::RECORD_TABLE_FIELD_TIME, 200);
+    headView->resizeSection(RecordTableField::RECORD_TABLE_FIELD_TYPE, 200);
+
+    m_ui->m_table->setItemDelegate(new DeviceRecordDelegate(this));
+
+    //just test add table data.
+    QList<RecordsInfo> infos;
+    for (int i = 0; i < 50; i++)
+    {
+        auto deviceInfo = RecordsInfo{.name = "1",
+                                      .type = i,
+                                      .time = "1",
+                                      .status = i};
+        infos << deviceInfo;
+    }
+    m_ui->m_table->setData(infos);
 }
 
 void DeviceRecord::searchTextChanged(const QString &text)
