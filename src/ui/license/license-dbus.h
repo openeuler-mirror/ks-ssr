834b2d6b5090b3d9ee5f6e02a923a85df5e0cf54
yuanxing <yuanxing@kylinsec.com.cn>
2023-05-11 18:56:18 +0800

    fix(license):fix code review problem
    - 完善代码审核问题

--- src/ui/license/license-dbus.h
+++ src/ui/license/license-dbus.h
@@ -31,22 +31,63 @@
 
 namespace KS
 {
+struct LicenseInfo
+{
+    LicenseInfo() : activationStatus(LicenseActivationStatus::LAS_LAST),
+                    expiredTime(0) {}
+
+    QString activationCode;
+    QString machineCode;
+    LicenseActivationStatus activationStatus;
+    time_t expiredTime;
+};
+
 class LicenseDBus : public QObject
 {
     Q_OBJECT
 public:
     LicenseDBus(QObject* parent = 0);
+    static QSharedPointer<LicenseDBus> getDefault();
     virtual ~LicenseDBus();
 
 public:
+    /**
+     * @brief creatObjectName: 创建激活对象
+     * @param objectName:激活对象名称
+     * @return true: 创建成功
+     *         false:创建失败
+     */
     bool creatObjectName(const QString& objectName);
-    QString getLicense();
+
+    /**
+     * @brief getLicense:获取授权信息
+     * @return 返回包含授权信息的结构体LicenseInfo
+     */
+    QSharedPointer<LicenseInfo> getLicense();
+
+    /**
+     * @brief activateByActivationCode:通过激活码授权
+     * @param activation_Code:激活码
+     * @param errorMsg:错误信息
+     * @return true: 授权成功
+     *         false:授权失败
+     */
     bool activateByActivationCode(const QString& activation_Code, QString& errorMsg);
 
+    /**
+     * @brief isActivate:判断是否授权
+     * @return true： 已授权
+     *         false：未授权/授权已过期
+     */
+    bool isActivated();
+
 signals:
-    void licenseChanged();
+    void licenseChanged(QSharedPointer<LicenseInfo>);
 
 private slots:
     void licenseChange(bool);
+
+private:
+    bool m_isActivated;
 };
 }  // namespace KS
