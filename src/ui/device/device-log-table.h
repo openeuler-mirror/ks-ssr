43d404c85d514e6f76e409325e16f865ec16e31d
yuanxing <yuanxing@kylinsec.com.cn>
2023-05-16 18:14:35 +0800

    fix(device):insert device list data into table from dbus
    - 从dbus接口中插入设备列表信息

--- src/ui/device/device-log-table.h
+++ src/ui/device/device-log-table.h
@@ -15,46 +15,60 @@
 #pragma once
 
 #include <QStandardItemModel>
+#include <QStyledItemDelegate>
 #include <QTableView>
+#include "src/ui/device_manager_proxy.h"
 
 namespace KS
 {
-struct DeviceInfo
+struct RecordInfo
 {
-    int number;
-    QString name;
-    int type;
-    QString id;
-    int interface;
-    int status;
-    int permission;
-};
-
-struct RecordsInfo
-{
+    RecordInfo() = default;
     QString name;
     int type;
     QString time;
     int status;
 };
 
+enum LogTableField
+{
+    LOG_TABLE_FIELD_NAME,
+    LOG_TABLE_FIELD_TYPE,
+    LOG_TABLE_FIELD_TIME,
+    LOG_TABLE_FIELD_STATUS,
+    LOG_TABLE_FIELD_LAST
+};
+
+class DeviceLogDelegate : public QStyledItemDelegate
+{
+    Q_OBJECT
+
+public:
+    DeviceLogDelegate(QObject *parent = 0);
+    virtual ~DeviceLogDelegate();
+    void paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const override;
+};
+
 class TableFilterModel;
-class DeviceListDelegate;
-class DeviceTable : public QTableView
+class DeviceLogTable : public QTableView
 {
     Q_OBJECT
 public:
-    DeviceTable(QWidget *parent = nullptr);
-    void setHeaderSections(QStringList sections);
-    void setDelegate(QAbstractItemDelegate *delegate);
-    void setData(const QList<DeviceInfo> &infos);
-    void setData(const QList<RecordsInfo> &infos);
+    DeviceLogTable(QWidget *parent = nullptr);
+    void update();
+    void setData(const QList<RecordInfo> &infos);
     int getColCount();
     int getRowCount();
     TableFilterModel *getFilterProxy();
 
 private:
+    void initTable();
+    void setHeaderSections(QStringList sections);
+
+private:
     TableFilterModel *m_filterProxy;
     QStandardItemModel *m_model;
+    DeviceManagerProxy *m_deviceManagerProxy;
+    QList<RecordInfo> m_recordsInfo;
 };
 }  // namespace KS
