c7b9b4031a1c5d3733f87970cbf4486311c1e404
wangyucheng <wangyucheng@kylinsec.com.cn>
2024-02-28 17:38:37 +0800

    feature(privacy cleaning): shred the user space when remove user.
    - 隐私清理时粉碎用户空间。
    - 修改实时告警的提示。
    - 优化删除用户或者粉碎文件时的日志和提示。

--- src/daemon/tool-box/manager.h
+++ src/daemon/tool-box/manager.h
@@ -32,6 +32,26 @@ namespace ToolBox
 {
 
 class RealTimeAlert;
+struct Group
+{
+    __gid_t gr_gid;     /* Group ID.	*/
+    QString gr_name;    /* Group name.	*/
+    QString gr_passwd;  /* Password.	*/
+    QStringList gr_mem; /* Member list.	*/
+};
+
+struct Passwd
+{
+    QString pw_name;   /* Username.  */
+    QString pw_passwd; /* Hashed passphrase, if shadow database
+                        not in use (see shadow.h).  */
+    __uid_t pw_uid;    /* User ID.  */
+    __gid_t pw_gid;    /* Group ID.  */
+    QString pw_gecos;  /* Real name.  */
+    QString pw_dir;    /* Home directory.  */
+    QString pw_shell;  /* Shell program.  */
+};
+
 
 class Manager : public QObject, public QDBusContext
 {
@@ -75,9 +95,9 @@ public:
 
     /**
      * @brief 调用 shred 来彻底删除文件的函数
-     * @param filePath 需要删除的文件的路径
+     * @param filePaths 需要删除的文件的路径
      */
-    void ShredFile(const QStringList& filePath);
+    void ShredFile(const QStringList& filePaths);
 
     /**
      * @brief 删除用户及其相关数据
@@ -100,6 +120,7 @@ public:
     void AddFileToFileShred(const QStringList&);
 
     void RemoveFileFromFileShred(const QStringList&);
+    bool removeFileFromFileShred(const QStringList&);
 
     static void hazardDetected(uint, const QString&);
 
@@ -116,6 +137,11 @@ private:
     void setFileMLSLabel(const QDBusMessage& message, const QString& filePath, const QString& SecurityContext);
     void setFileKICLabel(const QDBusMessage& message, const QString& filePath, const QString& SecurityContext);
     void setUserMLSLabel(const QDBusMessage& message, const QString& userName, const QString& SecurityContext);
+    void shredFile(const QDBusMessage& message, const QStringList& filePaths);
+    QStringList isPathsExist(const QStringList&);
+
+    // 实际调用 Shred 命令的函数， 此函数的参数文件列表中应该只有普通文件。
+    bool shred(const QStringList& filePaths);
 
 private:
     Manager();
@@ -126,25 +152,30 @@ private:
     bool setUserSeLabels(const QString& userName, const QString& seLabel, QString& output);
     bool getUserSeLabels(const QString& userName, QString& output);
     static void processFinishedHandler(Log::Log log, const int exitCode, const QProcess::ExitStatus exitStatus, const QSharedPointer<QProcess> cmd);
-    inline static QSharedPointer<QProcess> getProcess(Log::Log log, const QString& program, const QStringList& arg)
+    inline static QSharedPointer<QProcess> getProcess(const QString& program, const QStringList& arg, Log::Log log = Log::Log())
     {
         auto cmd = QSharedPointer<QProcess>::create();
         cmd->setProcessChannelMode(QProcess::MergedChannels);
         cmd->setProgram(program);
         cmd->setArguments(arg);
-        QObject::connect(cmd.data(),
-                         static_cast<void (QProcess::*)(int, QProcess::ExitStatus)>(&QProcess::finished),
-                         [cmd, log](int exitCode, QProcess::ExitStatus exitStatus) mutable
-                         {
-                             processFinishedHandler(log, exitCode, exitStatus, cmd);
-                         });
+        if (!log.logMsg.isEmpty())
+        {
+            QObject::connect(cmd.data(),
+                            static_cast<void (QProcess::*)(int, QProcess::ExitStatus)>(&QProcess::finished),
+                            [cmd, log](int exitCode, QProcess::ExitStatus exitStatus) mutable
+                            {
+                                processFinishedHandler(log, exitCode, exitStatus, cmd);
+                            });
+        }
         return cmd;
     }
 
-    void getAllUsers(const QString& path = "");
+    void updateAccountInfo(const QString& path = "");
 
 private:
     static Manager* m_toolBoxManager;
+    QList<Group> m_osGroupInfo;
+    QList<Passwd> m_osUserInfo;
     QString m_osUserInfoJson;
     QReadWriteLock* m_osUserNameMutex;
     QFileSystemWatcher* m_userNameWatcher;
