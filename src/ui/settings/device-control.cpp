8aa6e113c5fe3e3463b8686050f6a616aa598eff
wangyucheng <wangyucheng@kylinsec.com.cn>
2024-03-07 17:05:19 +0800

    fix(dm): Fix the wrong interface name - 修复错误的接口名称(#30759) - 没有名称的设备设置名称为”未知设备”(#30581) - 修复缺少设备连接记录的问题(#30468)

--- src/ui/settings/device-control.cpp
+++ src/ui/settings/device-control.cpp
@@ -16,9 +16,9 @@
 #include <QCheckBox>
 #include <QLabel>
 #include <QObject>
+#include "src/ui/common/user-prompt-dialog.h"
 #include "src/ui/common/ssr-marcos-ui.h"
 #include "src/ui/dm/utils.h"
-#include "src/ui/settings/respond-dialog.h"
 
 #define INTERFACE_TYPE_PROPERTY "interface type"
 
@@ -26,14 +26,14 @@ namespace KS
 {
 namespace Settings
 {
-DeviceControl::DeviceControl(QWidget *parent) : QWidget(parent),
-                                                m_deviceManagerProxy(nullptr),
-                                                m_gridLayout(nullptr),
-                                                m_usbLayout(nullptr),
-                                                m_kbdMouseLayout(nullptr),
-                                                m_kbdMouseContent(nullptr),
-                                                m_clickedCheckbox(nullptr),
-                                                m_respondDlg(nullptr)
+DeviceControl::DeviceControl(QWidget *parent)
+    : QWidget(parent),
+      m_deviceManagerProxy(nullptr),
+      m_gridLayout(nullptr),
+      m_usbLayout(nullptr),
+      m_kbdMouseLayout(nullptr),
+      m_kbdMouseContent(nullptr),
+      m_clickedCheckbox(nullptr)
 {
     initUI();
 
@@ -89,7 +89,7 @@ void DeviceControl::insertInterfaceWidget()
 
     foreach (auto interface, m_interfaces)
     {
-        if (interface.type <= InterfaceType::INTERFACE_TYPE_UNKNOWN ||
+        if (interface.type <= InterfaceType::INTERFACE_TYPE_OTHER ||
             interface.type >= InterfaceType::INTERFACE_TYPE_LAST)
         {
             continue;
@@ -185,17 +185,14 @@ QList<Interface> DeviceControl::getInterfaces()
 
 void DeviceControl::popupMessageDialog(const QString &text)
 {
-    if (!m_respondDlg)
-    {
-        m_respondDlg = new RespondDialog(this);
-        connect(m_respondDlg, &RespondDialog::accepted, this, &DeviceControl::accept);
-        connect(m_respondDlg, &RespondDialog::rejected, this, &DeviceControl::reject);
-    }
-    m_respondDlg->setMessage(text);
-    int x = window()->x() + window()->width() / 4 + m_respondDlg->width() / 4;
-    int y = window()->y() + window()->height() / 4 + m_respondDlg->height() / 8;
-    m_respondDlg->move(x, y);
-    m_respondDlg->show();
+    auto notify = new UserPromptDialog(this);
+    connect(notify, &UserPromptDialog::accepted, this, &DeviceControl::accept);
+    connect(notify, &UserPromptDialog::rejected, this, &DeviceControl::reject);
+    notify->setNotifyMessage(tr("Switch"), text);
+    auto x = window()->x() + window()->width() / 2 - notify->width() / 2;
+    auto y = window()->y() + window()->height() / 2 - notify->height() / 2;
+    notify->move(x, y);
+    notify->show();
 }
 
 void DeviceControl::setInterfaceState(bool checked)
