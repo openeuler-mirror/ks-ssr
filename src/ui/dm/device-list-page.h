8a4a6239660dc3bfbc0ffb275e678fa0074e47ae
wangyucheng <wangyucheng@kylinsec.com.cn>
2024-03-22 11:49:58 +0800

    fix(compile): fix compile warning - 修复编译告警。 - 移除一条代办事项， 审计告警规则配置沿用 auditctl ， 软件本身不提供配置功能。 - 移除 CI 的测试代码。

--- src/ui/dm/device-list-page.h
+++ src/ui/dm/device-list-page.h
@@ -41,13 +41,12 @@ public:
     QString getNavigationUID() override;
     QString getSidebarUID() override;
     QString getSidebarIcon() override;
-    int getSelinuxType() override;
+    QString getAccountRoleName() override;
 
 protected:
-    void paintEvent(QPaintEvent *event);
+    void paintEvent(QPaintEvent *event) override;
 
 private Q_SLOTS:
-    void searchTextChanged(const QString &text);
     void popupEditDialog(const QModelIndex &index);
     void updatePermission();
     void updateState();
