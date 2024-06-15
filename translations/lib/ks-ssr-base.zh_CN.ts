0e9093a8a15228a0ae2e8ca88e031a928dd5e968
yangfeng <yangfeng@kylinsec.com.cn>
2024-06-15 16:12:56 +0800

    feat（translations）：add object success invalid added - 后端支持报表功能

---
+++ translations/lib/ks-ssr-base.zh_CN.ts
@@ -0,0 +1,227 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>QObject</name>
+    <message>
+        <source>Success.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Invalid args.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Added file types are not supported.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The file is already in the list, and there is no need to add it repeatedly.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>There is no trusted card or the trusted card is not supported.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The pin code is wrong!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Failed to delete box.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Failed to create box.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Insufficient free space or unknown error, box creation failed.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>PrivateBox not found!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The box is exist!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The password set to the same as the current password is not supported.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Busy resources!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Failed to change the password, please check whether the password is correct.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The password complexity does not meet the requirements.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Password error!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Passphrase error!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Internal error!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Invalid device.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Invalid device permissions.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Invalid device interface type.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The graphics card does not support HDMI interface shutdown.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Password error.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>This account has been freeze.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>New password must be different from old password.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Permission denied.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Unknown account.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Failed to Change Multi-Factor authentication state.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>per page limit must less than 100 and page index must greater than 0.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Failed to set selinux status.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Failed to set mls Context, see log for more details.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Failed to get mls Context, see log for more details.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Failed to set kic Context, see log for more details.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Failed to get kic Context, see log for more details.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Failed to add obj to securitySign list, see log for more details.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Failed to remove users, see log for more details.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Failed to shred files, see log for more details.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Unknown error.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The standard type is invalid.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The strategy type is invalid.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The notification status is invalid.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The resource monitor is invalid.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Error format for reinforcement standard.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Reinforcement item &apos;{0}&apos; is not found.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The job is running, please don&apos;t repeat the operation.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The fallback is can&apos;t running, please wait for the reinforcement to be completed.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The standard reinforcement configuration is not found.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Machine code error.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Activation code error.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>There is no historical state, please reinforce it and operation.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The subsequest configuration item scan progress has been cancelled.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The subsequest configuration item reinforcement progress has been cancelled.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Internel error.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The software is not activated.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source> (error code: 0x%1)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
