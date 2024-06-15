c9bacb86731b4738053ab49ec70e33a961b407fa
yangfeng <yangfeng@kylinsec.com.cn>
2024-06-15 16:12:56 +0800

    feat（translations）：remove table vulnerability - 后端支持报表功能

--- translations/daemon/ks-ssr-daemon.zh_CN.ts
+++ translations/daemon/ks-ssr-daemon.zh_CN.ts
@@ -463,14 +463,6 @@
     </message>
 </context>
 <context>
-    <name>Table</name>
-    <message>
-        <location filename="../../lib/report/table.ui" line="98"/>
-        <source>Vulnerability information</source>
-        <translation type="unfinished"></translation>
-    </message>
-</context>
-<context>
     <name>UserPromptDialog</name>
     <message>
         <location filename="../../lib/widgets/user-prompt-dialog.ui" line="14"/>
