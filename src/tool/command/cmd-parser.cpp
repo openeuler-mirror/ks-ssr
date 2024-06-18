7ffc7166fb59a264e957a24d5480d953ab07d9c0
yangfeng <yangfeng@kylinsec.com.cn>
2024-06-18 16:54:10 +0800

    feat（command）：modify Command::repair: m dbus proxy exportreport vulnerabilityp... - 导出报表结束时,发送信号

--- src/tool/command/cmd-parser.cpp
+++ src/tool/command/cmd-parser.cpp
@@ -160,17 +160,22 @@ void Command::repair(const QStringList &cves)
 int Command::exportReport(QString which, QString path)
 {
     KLOG_DEBUG() << which << "exportPath:" << path;
-    auto reply = "br" == which ? m_dbusBRProxy->ExportReport(path) : m_dbusVulnerabilityProxy->ExportReport(path);
-    reply.waitForFinished();
-    if (reply.isError())
-    {
-        KLOG_WARNING() << "error:" << reply.error().message();
-        std::cout << tr("Failed to export report").toStdString() << std::endl;
-        exit(-1);
-    }
+    //    auto reply = "br" == which ? m_dbusBRProxy->ExportReport(path) : m_dbusVulnerabilityProxy->ExportReport(path);
+    //    reply.waitForFinished();
+    //    if (reply.isError())
+    //    {
+    //        KLOG_WARNING() << "error:" << reply.error().message();
+    //        std::cout << tr("Failed to export report").toStdString() << std::endl;
+    //        exit(-1);
+    //    }
 
-    std::cout << tr("Export Report Success").toStdString() << std::endl;
-    exit(0);
+    //    std::cout << tr("Export Report Success").toStdString() << std::endl;
+    //    exit(0);
+
+    connect(m_dbusBRProxy, &BRDbusProxy::ExportReportFinished, this, &Command::exportReportFinished, Qt::QueuedConnection);
+    connect(m_dbusVulnerabilityProxy, &VulnerabilityDbusProxy::ExportReportFinished, this, &Command::exportReportFinished, Qt::QueuedConnection);
+
+    "br" == which ? m_dbusBRProxy->ExportReport(path) : m_dbusVulnerabilityProxy->ExportReport(path);
 }
 
 QStringList Command::getBrInfo(const QStringList &category)
