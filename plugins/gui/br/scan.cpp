86f18decb7a24e239892e6155373fecb9f78606d
yangfeng <yangfeng@kylinsec.com.cn>
2024-06-19 11:34:37 +0800

    fix（br）：modify Scan::startReinforcement: m dbus proxy reinforceprogress ... - 修复命令行操作会触发界面响应

--- plugins/gui/br/scan.cpp
+++ plugins/gui/br/scan.cpp
@@ -479,7 +479,7 @@ void Scan::startReinforcement()
     CHECK_ERROR_FOR_DBUS_REPLY(reply);
     RETURN_IF_TRUE(reply.isError());
     // 设置页面回退会进行加固，为保证不起冲突，每次加固时断开后重新连接
-    disconnect(m_dbusProxy, SIGNAL(ReinforceProgress(QString)), nullptr, nullptr);
+    //    disconnect(m_dbusProxy, SIGNAL(ReinforceProgress(QString)), nullptr, nullptr);
     connect(m_dbusProxy, SIGNAL(ReinforceProgress(QString)), this, SLOT(runProgress(QString)));
     m_progressInfo.method = PROCESS_METHOD_FASTEN;
     clearState();
