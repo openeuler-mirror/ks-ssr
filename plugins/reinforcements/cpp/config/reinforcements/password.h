1d297ec89ec7bc351d1e71f7f4dd748e3d160be6
wangyucheng <wangyucheng@kylinsec.com.cn>
2024-06-19 14:36:41 +0800

    feature（reinforcements）：modify errorcode error code - 合并错误码。

--- plugins/reinforcements/cpp/config/reinforcements/password.h
+++ plugins/reinforcements/cpp/config/reinforcements/password.h
@@ -42,8 +42,8 @@ public:
     PasswordComplextiy(){};
     virtual ~PasswordComplextiy(){};
 
-    virtual bool get(std::string &args, BRErrorCode &error_code);
-    virtual bool set(const std::string &args, BRErrorCode &error_code);
+    virtual bool get(std::string &args, SSRErrorCode &error_code);
+    virtual bool set(const std::string &args, SSRErrorCode &error_code);
 
 private:
     std::shared_ptr<ConfigPAM> password_complextiy_config_;
