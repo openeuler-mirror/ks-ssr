16761d7658ee75e00fe69f4023020023df98ee2d
wangyucheng <wangyucheng@kylinsec.com.cn>
2024-06-19 14:36:41 +0800

    feature（reinforcements）：modify errorcode error code - 合并错误码。

--- plugins/reinforcements/cpp/network/reinforcements/firewalld.h
+++ plugins/reinforcements/cpp/network/reinforcements/firewalld.h
@@ -28,8 +28,8 @@ public:
     FirewalldSwitch();
     virtual ~FirewalldSwitch(){};
 
-    virtual bool get(std::string &args, BRErrorCode &error_code);
-    virtual bool set(const std::string &args, BRErrorCode &error_code);
+    virtual bool get(std::string &args, SSRErrorCode &error_code);
+    virtual bool set(const std::string &args, SSRErrorCode &error_code);
 
 private:
     std::shared_ptr<DBusSystemdProxy> systemd_proxy_;
