ad1907a4497c60087a95085820aac430bf27ae90
wangyucheng <wangyucheng@kylinsec.com.cn>
2024-06-19 14:36:41 +0800

    feature（br）：modify BRDBus::SetResourceMonitorSwitch: errorreply buserror inv... - 合并错误码。

--- plugins/daemon/br/dbus.cpp
+++ plugins/daemon/br/dbus.cpp
@@ -367,7 +367,7 @@ void BRDBus::SetResourceMonitorSwitch(const uint32_t& resourceMonitor)
 
     if (!this->m_configuration->setResourceMonitorStatus(BRResourceMonitor(resourceMonitor)))
     {
-        sendErrorReply(QDBusError::InvalidArgs, BR_ERROR2STR(BRErrorCode::ERROR_DAEMON_SET_RESOURCE_MONITOR_FAILED));
+        sendErrorReply(QDBusError::InvalidArgs, SSR_ERROR2STR(SSRErrorCode::ERROR_DAEMON_SET_RESOURCE_MONITOR_FAILED));
         SSR_LOG_ERROR(LogType::BASELINE_REINFORCEMENT,
                       tr("Failed to set resource monitor switch."),
                       calledUniqueName);
