251d85ebf9a5b7214de9c613408363fdb049f9f8
wangyucheng <wangyucheng@kylinsec.com.cn>
2024-06-21 17:24:20 +0800

    fix（command）：modify Command::repairProgress: m not exist vulnerabilityinfo cv... - 处理不存在的漏洞

--- src/tool/command/cmd-parser.cpp
+++ src/tool/command/cmd-parser.cpp
@@ -655,8 +655,8 @@ void Command::repairProgress(const QString &progress)
         QString id = cve.value("cveId").toString();
         if (!m_repairResult.contains(id))
         {
-            VulnerabilityInfo *pVu = new VulnerabilityInfo(id, getCveLevel(-1), QString("0"));
-            m_repairResult[id] = pVu;
+            m_notExistCVE << id;
+            continue;
         }
         m_repairResult.value(id)->state = getCveState(state);
     }
