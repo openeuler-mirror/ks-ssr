ea59c3214d631705911c2e0ec96d6e7a55e6c807
yangfeng <yangfeng@kylinsec.com.cn>
2024-06-21 14:28:57 +0800

    feature（translations）：modify report scannig result - 支持表头搜索和筛选

--- translations/gui/ks-ssr-gui-br.zh_CN.ts
+++ translations/gui/ks-ssr-gui-br.zh_CN.ts
@@ -418,114 +418,114 @@
     </message>
 </context>
 <context>
-    <name>KS::BR::Result</name>
+    <name>KS::BR::Report</name>
     <message>
-        <location filename="../../plugins/gui/br/reports/result.cpp" line="79"/>
-        <location filename="../../plugins/gui/br/reports/result.cpp" line="117"/>
+        <location filename="../../plugins/gui/br/reports/report.cpp" line="79"/>
+        <location filename="../../plugins/gui/br/reports/report.cpp" line="117"/>
         <source>Unknown</source>
-        <translation>未知</translation>
+        <translation type="unfinished">未知</translation>
     </message>
     <message>
-        <location filename="../../plugins/gui/br/reports/result.cpp" line="82"/>
-        <location filename="../../plugins/gui/br/reports/result.cpp" line="113"/>
+        <location filename="../../plugins/gui/br/reports/report.cpp" line="82"/>
+        <location filename="../../plugins/gui/br/reports/report.cpp" line="113"/>
         <source>Conformity</source>
-        <translation>符合</translation>
+        <translation type="unfinished">符合</translation>
     </message>
     <message>
-        <location filename="../../plugins/gui/br/reports/result.cpp" line="85"/>
-        <location filename="../../plugins/gui/br/reports/result.cpp" line="115"/>
+        <location filename="../../plugins/gui/br/reports/report.cpp" line="85"/>
+        <location filename="../../plugins/gui/br/reports/report.cpp" line="115"/>
         <source>Inconformity</source>
-        <translation>不符合</translation>
+        <translation type="unfinished">不符合</translation>
     </message>
     <message>
-        <location filename="../../plugins/gui/br/reports/result.cpp" line="88"/>
+        <location filename="../../plugins/gui/br/reports/report.cpp" line="88"/>
         <source>Not Scanned</source>
-        <translation>未扫描</translation>
+        <translation type="unfinished">未扫描</translation>
     </message>
     <message>
-        <location filename="../../plugins/gui/br/reports/result.cpp" line="94"/>
-        <source>Scan Failed</source>
-        <translation>扫描失败</translation>
-    </message>
-    <message>
-        <location filename="../../plugins/gui/br/reports/result.cpp" line="97"/>
-        <source>Scan Complete</source>
-        <translation>扫描完成</translation>
-    </message>
-    <message>
-        <location filename="../../plugins/gui/br/reports/result.cpp" line="100"/>
-        <source>Unreinforcement</source>
-        <translation>未加固</translation>
-    </message>
-    <message>
-        <location filename="../../plugins/gui/br/reports/result.cpp" line="91"/>
+        <location filename="../../plugins/gui/br/reports/report.cpp" line="91"/>
         <source>Scannig...</source>
-        <translation>正在扫描...</translation>
+        <translation type="unfinished">正在扫描...</translation>
     </message>
     <message>
-        <location filename="../../plugins/gui/br/reports/result.cpp" line="103"/>
+        <location filename="../../plugins/gui/br/reports/report.cpp" line="94"/>
+        <source>Scan Failed</source>
+        <translation type="unfinished">扫描失败</translation>
+    </message>
+    <message>
+        <location filename="../../plugins/gui/br/reports/report.cpp" line="97"/>
+        <source>Scan Complete</source>
+        <translation type="unfinished">扫描完成</translation>
+    </message>
+    <message>
+        <location filename="../../plugins/gui/br/reports/report.cpp" line="100"/>
+        <source>Unreinforcement</source>
+        <translation type="unfinished">未加固</translation>
+    </message>
+    <message>
+        <location filename="../../plugins/gui/br/reports/report.cpp" line="103"/>
         <source>Reinforcing...</source>
-        <translation>正在加固...</translation>
+        <translation type="unfinished">正在加固...</translation>
     </message>
     <message>
-        <location filename="../../plugins/gui/br/reports/result.cpp" line="106"/>
+        <location filename="../../plugins/gui/br/reports/report.cpp" line="106"/>
         <source>Reinforcement Failure</source>
-        <translation>加固失败</translation>
+        <translation type="unfinished">加固失败</translation>
     </message>
     <message>
-        <location filename="../../plugins/gui/br/reports/result.cpp" line="109"/>
+        <location filename="../../plugins/gui/br/reports/report.cpp" line="109"/>
         <source>Reinforced</source>
-        <translation>加固成功</translation>
+        <translation type="unfinished">加固成功</translation>
     </message>
     <message>
-        <location filename="../../plugins/gui/br/reports/result.cpp" line="180"/>
+        <location filename="../../plugins/gui/br/reports/report.cpp" line="180"/>
         <source>No master file</source>
-        <translation>无属主文件</translation>
+        <translation type="unfinished">无属主文件</translation>
     </message>
     <message>
-        <location filename="../../plugins/gui/br/reports/result.cpp" line="186"/>
+        <location filename="../../plugins/gui/br/reports/report.cpp" line="186"/>
         <source>Files with 777 permissions</source>
-        <translation>文件具有777权限</translation>
+        <translation type="unfinished">文件具有777权限</translation>
     </message>
     <message>
-        <location filename="../../plugins/gui/br/reports/result.cpp" line="197"/>
+        <location filename="../../plugins/gui/br/reports/report.cpp" line="197"/>
         <source>Files with sgid permission</source>
-        <translation>文件具有sgid权限</translation>
+        <translation type="unfinished">文件具有sgid权限</translation>
     </message>
     <message>
-        <location filename="../../plugins/gui/br/reports/result.cpp" line="199"/>
+        <location filename="../../plugins/gui/br/reports/report.cpp" line="199"/>
         <source>Files with suid permission</source>
-        <translation>文件具有suid权限</translation>
+        <translation type="unfinished">文件具有suid权限</translation>
     </message>
     <message>
-        <location filename="../../plugins/gui/br/reports/result.cpp" line="291"/>
+        <location filename="../../plugins/gui/br/reports/report.cpp" line="291"/>
         <source>Unactivated</source>
-        <translation>未激活</translation>
+        <translation type="unfinished">未激活</translation>
     </message>
     <message>
-        <location filename="../../plugins/gui/br/reports/result.cpp" line="291"/>
+        <location filename="../../plugins/gui/br/reports/report.cpp" line="291"/>
         <source>Activated</source>
-        <translation>已激活</translation>
+        <translation type="unfinished">已激活</translation>
     </message>
     <message>
-        <location filename="../../plugins/gui/br/reports/result.cpp" line="402"/>
+        <location filename="../../plugins/gui/br/reports/report.cpp" line="402"/>
         <source>Vulnerability exists in version %1</source>
-        <translation>版本号%1存在漏洞</translation>
+        <translation type="unfinished">版本号%1存在漏洞</translation>
     </message>
     <message>
-        <location filename="../../plugins/gui/br/reports/result.cpp" line="465"/>
+        <location filename="../../plugins/gui/br/reports/report.cpp" line="465"/>
         <source>KylinSecHostReinforcementReport_%1_%2.pdf</source>
-        <translation>麒麟信安主机安全加固报告_%1_%2.pdf</translation>
+        <translation type="unfinished">麒麟信安主机安全加固报告_%1_%2.pdf</translation>
     </message>
     <message>
-        <location filename="../../plugins/gui/br/reports/result.cpp" line="466"/>
+        <location filename="../../plugins/gui/br/reports/report.cpp" line="466"/>
         <source>export report</source>
-        <translation>导出报告</translation>
+        <translation type="unfinished">导出报告</translation>
     </message>
     <message>
-        <location filename="../../plugins/gui/br/reports/result.cpp" line="466"/>
+        <location filename="../../plugins/gui/br/reports/report.cpp" line="466"/>
         <source>PDF(*.pdf)</source>
-        <translation></translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
