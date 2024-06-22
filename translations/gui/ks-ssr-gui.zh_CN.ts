73c461fc79b9b1199ede8ade28f5e699c4fa13d9
wangyucheng <wangyucheng@kylinsec.com.cn>
2024-06-22 14:53:54 +0800

    fix（translations）：remove polkitproxy polkit proxy authorization - 添加翻译。

--- translations/gui/ks-ssr-gui.zh_CN.ts
+++ translations/gui/ks-ssr-gui.zh_CN.ts
@@ -365,14 +365,6 @@
     </message>
 </context>
 <context>
-    <name>PolkitProxy</name>
-    <message>
-        <location filename="../../lib/dbus/polkit-proxy.cpp" line="218"/>
-        <source>Authorization failed.</source>
-        <translation>认证失败。</translation>
-    </message>
-</context>
-<context>
     <name>QObject</name>
     <message>
         <location filename="../../lib/base/error.cpp" line="34"/>
