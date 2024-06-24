c0f289d9b74e8181e602a964c6e1cc0dc6c14758
tangjie02 <tangjie02@kylinsec.com.cn>
2024-06-24 11:18:44 +0800

    feature（base）：add Error::getErrorDesc: code changes - 将扫描、加固和回退功能剥离到单独的任务管理类中

--- lib/base/error.cpp
+++ lib/base/error.cpp
@@ -58,6 +58,7 @@ QString Error::getErrorDesc(SSRErrorCode errorCode)
         break;
     case ERROR_DAEMON_SCAN_IS_RUNNING:
     case ERROR_DAEMON_REINFORCE_IS_RUNNING:
+    case ERROR_BR_FALLBACK_IS_RUNNING:
         errorDesc = QObject::tr("The job is running, please don't repeat the operation.");
         break;
     case ERROR_DAEMON_FALLBACK_CANNOT_RUNNING:
