494a81f63cb477d706a0aada1b7091a74dc34188
tangjie02 <tangjie02@kylinsec.com.cn>
2024-06-25 14:32:30 +0800

    feature（external）：modify br sshd port br sshd sftp ks ssr - 修改回退代码逻辑，为每个加固项增加备份和回退接口

--- plugins/reinforcements/python/br/external/sshd.py
+++ plugins/reinforcements/python/br/external/sshd.py
@@ -6,8 +6,8 @@ import br.systemd
 import br.log
 
 SSHD_CONF_PATH = "/etc/ssh/sshd_config"
-SELINUX_MODULES_PORT_PATH = "/usr/share/ks-ssr/br-sshd-port.pp"
-SELINUX_MODULES_SFTP_PATH = "/usr/share/ks-ssr/br-sshd-sftp.pp"
+SELINUX_MODULES_PORT_PATH = br.vars.SSR_BR_INSTALL_DATADIR + "/br-sshd-port.pp"
+SELINUX_MODULES_SFTP_PATH = br.vars.SSR_BR_INSTALL_DATADIR + "/br-sshd-sftp.pp"
 
 # 允许root进行ssh远程登陆
 ROOT_LOGIN_ARG_ENABLED = "enabled"
