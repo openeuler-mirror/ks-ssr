ae133395ab90a7bda8a20e5ab9c94c01049ace2e
tangjie02 <tangjie02@kylinsec.com.cn>
2024-06-25 14:32:30 +0800

    feature（python）：add string - 修改回退代码逻辑，为每个加固项增加备份和回退接口

--- plugins/daemon/br/python/plugin-python.h
+++ plugins/daemon/br/python/plugin-python.h
@@ -35,6 +35,8 @@ public:
 
     virtual bool get(QString &args, QString &error);
     virtual bool set(const QString &args, QString &error);
+    virtual bool backup(QString &args, QString &error);
+    virtual bool rollback(const QString &args, QString &error);
 
     bool isValid()
     {
