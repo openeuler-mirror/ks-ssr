a23856628d1e44a67afd3fbd8cc102978d913cd0
tangjie02 <tangjie02@kylinsec.com.cn>
2024-06-25 14:32:30 +0800

    feature（br）：add args json - 修改回退代码逻辑，为每个加固项增加备份和回退接口

--- plugins/reinforcements/python/br/config/sysctl.py
+++ plugins/reinforcements/python/br/config/sysctl.py
@@ -158,3 +158,8 @@ class KeyRebootSwitch:
                 self.close()
 
         return (True, '')
+
+    def backup(self):
+        return self.get()
+    def rollback(self, args_json):
+        return self.set(args_json)
