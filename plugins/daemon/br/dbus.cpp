fdea5116b5742fb56f7cd906ace76da7fcea39d3
tangjie02 <tangjie02@kylinsec.com.cn>
2024-06-27 13:46:39 +0800

    feature（br）：modify BRDBus::Scan: errorreply buserror internalerror errorcode - 清理未使用的错误码，重命名基线加固的错误码前缀名称

--- plugins/daemon/br/dbus.cpp
+++ plugins/daemon/br/dbus.cpp
@@ -555,7 +555,7 @@ void BRDBus::Scan(const QStringList& names)
 
     if (m_jobDispatcher->getState() != BRDispatchState::BR_DISPATCH_STATE_IDLE)
     {
-        sendErrorReply(QDBusError::InternalError, SSR_ERROR2STR(SSRErrorCode::ERROR_BR_JOB_IS_RUNNING));
+        sendErrorReply(QDBusError::InternalError, SSR_ERROR2STR(SSRErrorCode::ERROR_PLUGIN_BR_JOB_IS_RUNNING));
         return;
     }
 
