691b164d7243ee039013b4663617c587be562976
tangjie02 <tangjie02@kylinsec.com.cn>
2024-06-27 13:46:39 +0800

    feature（br）：modify BRDBus::SetTimeScan: errorreply buserror internalerror er... - 清理未使用的错误码，重命名基线加固的错误码前缀名称

--- plugins/daemon/br/dbus.cpp
+++ plugins/daemon/br/dbus.cpp
@@ -260,7 +260,7 @@ void BRDBus::SetTimeScan(const uint32_t& timeScan)
     }
     if (!this->m_configuration->setTimeScan(int(timeScan)))
     {
-        sendErrorReply(QDBusError::InternalError, SSR_ERROR2STR(SSRErrorCode::ERROR_DAEMON_SET_TIME_SCAN_FAILED));
+        sendErrorReply(QDBusError::InternalError, SSR_ERROR2STR(SSRErrorCode::ERROR_PLUGIN_BR_SET_TIME_SCAN_FAILED));
         SSR_LOG_ERROR(LogType::BASELINE_REINFORCEMENT,
                       tr("Failed to set time scan."),
                       calledUniqueName);
