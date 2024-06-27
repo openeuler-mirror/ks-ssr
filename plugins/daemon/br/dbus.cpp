5955a7ae371c46b7d40cab6f7acb9d28bcd30bc4
tangjie02 <tangjie02@kylinsec.com.cn>
2024-06-27 13:46:39 +0800

    feature（br）：modify BRDBus::SetReinforcements: errorcode - 清理未使用的错误码，重命名基线加固的错误码前缀名称

--- plugins/daemon/br/dbus.cpp
+++ plugins/daemon/br/dbus.cpp
@@ -501,7 +501,7 @@ void BRDBus::SetReinforcements(const QString& reinforcements)
     if (m_configuration->getStrategyType() == BRStrategyType::BR_STRATEGY_TYPE_SYSTEM)
     {
         sendErrorReply(QDBusError::NotSupported,
-                       SSR_ERROR2STR(SSRErrorCode::ERROR_BR_NEED_CUSTOM_STRATEGY_TYPE));
+                       SSR_ERROR2STR(SSRErrorCode::ERROR_PLUGIN_BR_NEED_CUSTOM_STRATEGY_TYPE));
         return;
     }
 
