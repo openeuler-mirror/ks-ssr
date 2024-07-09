2f77567e02df4bc5277d591b8f580db846dc2e88
tangjie02 <tangjie02@kylinsec.com.cn>
2024-07-09 17:34:15 +0800

    feature（br）：remove code changes - 修改config-umask-limit加固项，并为该加固项增加MaskRule规则，去掉对config-umask-limit的自定义代码

--- plugins/reinforcements/python/br/utils.py
+++ plugins/reinforcements/python/br/utils.py
@@ -42,8 +42,6 @@ def subprocess_has_output_ignore_error_handling(args):
         args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
 
     exit_code = child_process.wait()
-    # 暂时通过添加sleep方式解决cpu占用率过高的问题（#57871）
-    # time.sleep(0.05)
 
     error = child_process.stderr.read().strip().decode('utf-8')
     if exit_code != 0 and len(error) > 0:
