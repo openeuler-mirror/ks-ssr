10699c04733323b90be9fca94f8dbe38deee7504
yangfeng <yangfeng@kylinsec.com.cn>
2024-07-11 17:40:36 +0800

    fix（command）：modify Command::addDbusServerWatcher: m dbus serverwatcher busse... - 修正sonarqube监测出的bug

--- src/tool/command/cmd-parser.cpp
+++ src/tool/command/cmd-parser.cpp
@@ -230,7 +230,7 @@ void Command::addDbusServerWatcher()
             m_dbusServerWatcher->setConnection(connection);
             m_dbusServerWatcher->setWatchMode(QDBusServiceWatcher::WatchForOwnerChange);
             m_dbusServerWatcher->addWatchedService(reply.value());
-            connect(m_dbusServerWatcher, &QDBusServiceWatcher::serviceUnregistered, [this](const QString &service)
+            connect(m_dbusServerWatcher, &QDBusServiceWatcher::serviceUnregistered, [](const QString &service)
                     {
                         std::cout << tr("The background daemon service exits. The unique name of the dbus service: ").toStdString() << service.toStdString() << std::endl;
                         exit(-1);
