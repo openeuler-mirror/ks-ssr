519ac1f7a85c34f3dc1e0060f99f04c6b6ee4c50
yangfeng <yangfeng@kylinsec.com.cn>
2024-07-11 16:08:47 +0800

    feature（command）：add license proxy - 优化帮助信息

--- src/tool/command/cmd-parser.cpp
+++ src/tool/command/cmd-parser.cpp
@@ -22,6 +22,7 @@
 #include "include/ssr-i.h"
 #include "lib/base/str-utils.h"
 #include "lib/base/sys-info.h"
+#include "lib/dbus/license-proxy.h"
 #include "qt5-log-i.h"
 #include "vulnerability_dbus_proxy.h"
 
