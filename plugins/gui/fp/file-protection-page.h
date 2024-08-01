52d8240eb3e02b8be4b402cd3ee00f971d5735a9
yangfeng <yangfeng@kylinsec.com.cn>
2024-08-01 15:37:59 +0800

    feature（fp）：add taskrunning task - 完善所有插件退出函数和修改不能编译通过的代码

--- plugins/gui/fp/file-protection-page.h
+++ plugins/gui/fp/file-protection-page.h
@@ -38,6 +38,8 @@ public:
     QString getSidebarUID() override;
     QString getSidebarIcon() override;
     QString getAccountRoleName() override;
+    bool isTaskRunning() override;
+    bool stopTask() override;
 
 private:
     bool checkTrustedLoadFinied(bool initialized);
