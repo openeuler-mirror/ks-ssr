3ff33641ed51a33b51015a19f7fb1573dd3f8a40
yangfeng <yangfeng@kylinsec.com.cn>
2024-08-01 15:37:59 +0800

    feature（br）：modify taskrunning canexit - 完善所有插件退出函数和修改不能编译通过的代码

--- plugins/gui/br/br-page.h
+++ plugins/gui/br/br-page.h
@@ -43,7 +43,7 @@ public:
     QString getSidebarUID() override;
     QString getSidebarIcon() override;
     QString getAccountRoleName() override;
-    bool checkCanExit() override;
+    bool isTaskRunning() override;
     bool stopTask() override;
 
     // 导出策略需要从表格中获取勾选项，设置页面中无法获取，通过信号实现
