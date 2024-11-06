7a890718ff98283ec410b445049d26155ba0fcb5
yangfeng <yangfeng@kylinsec.com.cn>
2024-11-06 14:18:18 +0800

    fix（br）：modify call noresult - 修复加固后服务没有自启问题

--- plugins/reinforcements/python/br/service_manager.py
+++ plugins/reinforcements/python/br/service_manager.py
@@ -169,12 +169,12 @@ class Initd(ServiceManager):
     def start(self):
         if self.is_active():
             return
-        self.__call_noresult('start')
+        self.__call_noresult("start")
 
     def stop(self):
         if not self.is_active():
             return
-        self.__call_noresult('stop')
+        self.__call_noresult("stop")
 
     # initd 中实现 mask/umask 功能比较麻烦， 现有加固项暂时没有使用，暂不实现
     def mask(self):
