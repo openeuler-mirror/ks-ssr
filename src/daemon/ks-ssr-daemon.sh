6183f604378f689cf97696a5bf5e4c1edc8ddd00
wangyucheng <wangyucheng@kylinsec.com.cn>
2025-02-24 14:56:59 +0800

    fix（daemon）：add ks ssr - 修正在 centos6 上 gio/module 路径的环境变量。

--- src/daemon/ks-ssr-daemon.sh
+++ src/daemon/ks-ssr-daemon.sh
@@ -4,5 +4,7 @@
 basedir=/usr/libexec
 
 #export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/lib64/ks-ssr/lib64"
-export GIO_EXTRA_MODULES='/root/gio/modules'
+export GIO_EXTRA_MODULES='/usr/lib64/ks-ssr/lib64/gio/modules/'
+export GIO_MODULE_DIR='/usr/lib64/ks-ssr/lib64/gio/modules/'
+export PYTHONPATH="/usr/lib64/ks-ssr/lib64/python/lib-dynload/:/usr/lib64/ks-ssr/lib/python/lib-dynload/:/usr/lib64/ks-ssr/lib64/python/:/usr/lib64/ks-ssr/lib64/python/site-packages/:/usr/lib64/ks-ssr/lib/python/:/usr/lib64/ks-ssr/lib/python/site-packages/:${PYTHONPATH}"
 exec "$basedir"/ks-ssr-daemon
